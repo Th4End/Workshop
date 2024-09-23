933e63bc092d0ce81106cdf3063f09a163fd5448
Alexia-N27 <al.nicoleau27@gmail.com>
2024-09-23 16:57:57 +0200

    Mise en place router

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <HeaderWork />
+    <h1>Bienvenue sur la page d'accueil !</h1>
+    <p>Ceci est votre page principale.</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
